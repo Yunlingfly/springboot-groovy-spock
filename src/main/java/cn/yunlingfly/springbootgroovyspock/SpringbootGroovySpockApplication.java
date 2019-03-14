3d4ba006317dc109e16abcfaef43da425fbe6472
yunlingfly <508821881@qq.com>
2019-03-14 10:36:27 +0800

    [init]

---
+++ src/main/java/cn/yunlingfly/springbootgroovyspock/SpringbootGroovySpockApplication.java
@@ -0,0 +1,13 @@
+package cn.yunlingfly.springbootgroovyspock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGroovySpockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGroovySpockApplication.class, args);
+    }
+
+}
