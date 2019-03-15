5eab2cdd45b51691f39d20468054c3a295434a93
yunlingfly <508821881@qq.com>
2019-03-15 10:27:32 +0800

    [FIX]getOtherById

--- src/main/java/cn/yunlingfly/springbootgroovyspock/controller/TestController.groovy
+++ src/main/java/cn/yunlingfly/springbootgroovyspock/controller/TestController.groovy
@@ -1,16 +1,27 @@
 package cn.yunlingfly.springbootgroovyspock.controller
 
 import cn.yunlingfly.springbootgroovyspock.bean.Cc
+import cn.yunlingfly.springbootgroovyspock.bean.Other
+import cn.yunlingfly.springbootgroovyspock.service.IOtherService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class TestController {
+    @Autowired
+    IOtherService iOtherService;
+
     @RequestMapping(value = "/hello")
     Cc hello(){
         Cc cc=new Cc()
         cc.name="myName"
         cc.version="0.1"
         return cc
+    }
+
+    @RequestMapping(value = "/getOtherById")
+    Other getOtherById(Integer id){
+        return iOtherService.getOtherById(id);
     }
 }
--- src/test/groovy/cn/yunlingfly/springbootgroovyspock/controller/TestControllerTests.groovy
+++ src/test/groovy/cn/yunlingfly/springbootgroovyspock/controller/TestControllerTests.groovy
@@ -1,14 +1,21 @@
 package cn.yunlingfly.springbootgroovyspock.controller
 
 import cn.yunlingfly.springbootgroovyspock.bean.Cc
+import cn.yunlingfly.springbootgroovyspock.bean.Other
+import cn.yunlingfly.springbootgroovyspock.service.IOtherService
 import com.alibaba.fastjson.JSON
 import org.junit.Assert
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.test.context.junit4.SpringRunner
+import spock.lang.Shared
 import spock.lang.Specification
+
+import java.lang.reflect.Field
 
 // 使用Spock测试框架框架就不能使用下面的注解否则报错No runnable methods
 //@RunWith(SpringRunner.class)
@@ -18,7 +25,19 @@ class TestControllerTests extends Specification{
     @Autowired
     private TestRestTemplate testRestTemplate
 
-    void '测试/hello'() {
+    TestController testController=new TestController()
+    @Autowired
+    IOtherService otherService;
+
+    @Shared
+    def Other other
+
+    void setup() {
+        otherService=Mock(IOtherService)
+        testController.iOtherService=otherService
+    }
+
+    def '测试/hello'() {
         given: '定义请求数据格式'
         def searchParam = ""
 
@@ -32,5 +51,22 @@ class TestControllerTests extends Specification{
 //        Cc context = testRestTemplate.getForObject("/hello",Cc.class);
 //        println(JSON.toJSONString(context))
 //        Assert.assertEquals('myName', context.name)
+    }
+
+    def '测试/getOtherById'() {
+        given: '定义请求数据格式'
+        // 初始化我们测试接口返回的数据
+        other = new Other()
+        other.setId(1)
+        other.setName("myName")
+        other.setEnabled(true)
+
+        when: '向接口发请求'
+        otherService.getOtherById(_) >> other
+
+        then: '状态码为200'
+        Other other1 = testController.getOtherById(2)
+        println(JSON.toJSONString(other1))
+        Assert.assertEquals('myName', other1.name)
     }
 }
