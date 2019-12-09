64285b2f09858224cd4b0c75941e7a0693536f78
yunlingfly <508821881@qq.com>
2019-12-09 14:49:28 +0800

    [ADD] author&date

--- src/main/java/cn/yunlingfly/springbootgroovyspock/SpringbootGroovySpockApplication.java
+++ src/main/java/cn/yunlingfly/springbootgroovyspock/SpringbootGroovySpockApplication.java
@@ -3,6 +3,10 @@ package cn.yunlingfly.springbootgroovyspock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yunlingfly
+ * @date 2019/12/9
+ */
 @SpringBootApplication
 public class SpringbootGroovySpockApplication {
 
